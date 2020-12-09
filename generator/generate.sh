53553db9dc9fa85ebd17686abd009f88e6c123f3
multicatch <cezaryre@gmail.com>
2020-12-09 11:56:15 +0100

    Added github action to generate wiki pages

---
+++ generator/generate.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+java -jar fuddi-wiki-generator.jar \
+	-source-file ./ontology/Food.rdf \
+	-template-file ./generator/generator.txt \
+	-wikimedia.url https://kb.fuddi.eu/api.php \
+	-wikimedia.login "${WIKIMEDIA_LOGIN}" \
+	-wikimedia.password "${WIKIMEDIA_PASSWORD}"
