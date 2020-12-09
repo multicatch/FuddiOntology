6e347aa34cd5a736fe3c5d40be82c8b54e18609e
multicatch <cezaryre@gmail.com>
2020-12-09 12:02:53 +0100

    Fixed the generator script

--- generate.sh
+++ generate.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-java -jar fuddi-wiki-generator.jar \
+java -jar ./generator/fuddi-wiki-generator.jar \
 	-source-file ./ontology/Food.rdf \
 	-template-file ./generator/generator.txt \
 	-wikimedia.url https://kb.fuddi.eu/api.php \
